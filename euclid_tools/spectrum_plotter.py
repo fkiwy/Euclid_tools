39b176e03f6f0dbf2c5a464a5ae1a414a63ef37d
Frank Kiwy <54667906+fkiwy@users.noreply.github.com>
2025-04-21 10:37:08 +0200

    Change appearance of spectrum plot

--- euclid_tools/spectrum_plotter.py
+++ euclid_tools/spectrum_plotter.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import euclid_tools.shared as shr
 
 
-def plot_spectrum(data, ra, dec, output_dir=tempfile.gettempdir(), open_plot=True, plot_format="png"):
+def plot_spectrum(
+    data, ra, dec, output_dir=tempfile.gettempdir(), open_plot=True, plot_format="png"
+):
     """
     Plot and save the spectrum of a source at given coordinates.
 
@@ -34,21 +36,24 @@ def plot_spectrum(data, ra, dec, output_dir=tempfile.gettempdir(), open_plot=Tru
         - Assumes the "WAVELENGTH", "FLUX", and "ERROR" columns are available and properly formatted.
     """
 
-    object_name = shr.create_object_name(ra, dec, precision=2, shortform=False, prefix="J", decimal=False)
+    object_name = shr.create_object_name(
+        ra, dec, precision=2, shortform=False, prefix="J", decimal=False
+    )
     filename = os.path.join(output_dir, object_name + "_spectrum." + plot_format)
 
     wavelength = data["WAVELENGTH"]
     flux = data["FLUX"]
     error = data["ERROR"]
 
-    plt.rcParams.update({"font.family": "Arial"})
-    plt.plot(wavelength, flux, color="black", label="Spectrum")
-    plt.plot(wavelength, error, color="red", label="Error")
-    plt.xlabel(f"Wavelength [{to_latex(wavelength.unit)}]")
-    plt.ylabel(f"Flux [{to_latex(flux.unit)}]")
+    plt.figure(figsize=(8, 6))
+    plt.rcParams.update(**_get_plot_settings())
+    plt.plot(wavelength, flux, color="k", alpha=0.5, label="Spectrum")
+    plt.plot(wavelength, error, color="r", alpha=0.5, label="Error")
+    plt.xlabel(f"Wavelength [{_to_latex(wavelength.unit)}]")
+    plt.ylabel(f"Flux [{_to_latex(flux.unit)}]")
     plt.title(object_name)
-    legend =plt.legend(loc="best")
-    legend.get_frame().set_boxstyle('Square')
+    legend = plt.legend(loc="best")
+    legend.get_frame().set_boxstyle("Square")
     plt.savefig(filename, dpi=300, bbox_inches="tight", format=plot_format)
     plt.close()
 
@@ -56,5 +61,27 @@ def plot_spectrum(data, ra, dec, output_dir=tempfile.gettempdir(), open_plot=Tru
         shr.open_file(filename)
 
 
-def to_latex(unit):
+def _get_plot_settings():
+    return {
+        "font.family": "Arial",  # Set the font family
+        "font.size": 12,  # Set the font size for labels and title
+        "axes.linewidth": 1.0,  # Make axis lines thicker
+        "xtick.major.size": 5.0,  # Set major x-tick size
+        "xtick.minor.size": 3.0,  # Set minor x-tick size
+        "xtick.major.width": 1.0,  # Set major x-tick line width
+        "xtick.minor.width": 1.0,  # Set minor x-tick line width
+        "xtick.direction": "in",  # Set x-ticks to point inward
+        "xtick.minor.visible": True,  # Make minor ticks visible
+        "xtick.top": True,  # Make top x-ticks visible
+        "ytick.major.size": 5.0,  # Set major y-tick size
+        "ytick.minor.size": 3.0,  # Set minor y-tick size
+        "ytick.major.width": 1.0,  # Set major y-tick line width
+        "ytick.minor.width": 1.0,  # Set minor y-tick line width
+        "ytick.direction": "in",  # Set y-ticks to point inward
+        "ytick.minor.visible": True,  # Make minor ticks visible
+        "ytick.right": True,  # Make right y-ticks visible
+    }
+
+
+def _to_latex(unit):
     return unit.to_string("latex_inline")
